72419230bb0c9b9fcd7f0289e897985800bbdbbd
Ansel Castro <ansel86castro@gmail.com>
2021-04-07 22:34:10 -0300

    Assembly Loader

---
+++ src/Cybtans.Proto.Test/AssemblyLoaderTest.cs
@@ -0,0 +1,20 @@
+﻿using Cybtans.Proto.Utils;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Xunit;
+
+namespace Cybtans.Proto.Test
+{
+    public class AssemblyLoaderTest
+    {
+        [Fact]
+        public void LoadAssembly()
+        {
+           var assembly =new AssemblyLoader("D:/Projects/CybtansSDK/cybtans-sdk/CybtansSDK/Tests/Cybtans.Tests.Domain/bin/Debug/netcoreapp3.1/Cybtans.Tests.Domain.dll");
+            var types = assembly.LoadTypes();
+            
+        }
+    }
+}
