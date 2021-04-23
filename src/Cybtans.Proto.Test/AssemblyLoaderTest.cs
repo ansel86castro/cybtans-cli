6a3c544a27ddac4164c9318e7ffea614c5a07e24
Ansel Castro <ansel86castro@gmail.com>
2021-04-23 01:30:41 -0300

    Update AssemblyLoaderTest.cs

--- src/Cybtans.Proto.Test/AssemblyLoaderTest.cs
+++ src/Cybtans.Proto.Test/AssemblyLoaderTest.cs
@@ -9,12 +9,12 @@ namespace Cybtans.Proto.Test
 {
     public class AssemblyLoaderTest
     {
-        [Fact]
-        public void LoadAssembly()
-        {
-           var assembly =new AssemblyLoader("D:/Projects/CybtansSDK/cybtans-sdk/CybtansSDK/Tests/Cybtans.Tests.Domain/bin/Debug/netcoreapp3.1/Cybtans.Tests.Domain.dll");
-            var types = assembly.LoadTypes();
+        //[Fact]
+        //public void LoadAssembly()
+        //{
+        //   var assembly =new AssemblyLoader("D:/Projects/CybtansSDK/cybtans-sdk/CybtansSDK/Tests/Cybtans.Tests.Domain/bin/Debug/netcoreapp3.1/Cybtans.Tests.Domain.dll");
+        //    var types = assembly.LoadTypes();
             
-        }
+        //}
     }
 }
